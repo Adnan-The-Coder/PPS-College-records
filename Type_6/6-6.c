8850b31279b34d4ba5c5ce5470505991ba862d05
Adnan <adnanali23189@gmail.com>
2024-11-30 07:30:18 +0530

    update 6-6 Program

--- Type_6/6-6.c
+++ Type_6/6-6.c
@@ -12,22 +12,25 @@ int linearSearch(int arr[], int n, int key) {
 
 // Function for Binary Search (requires sorted array)
 int binarySearch(int arr[], int n, int key) {
-    int left = 0, right = n - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
-        if (arr[mid] == key) {
-            return mid;  // Return the index where the key is found
-        }
-        else if (arr[mid] < key) {
-            left = mid + 1;  // Narrow the search to the right half
-        }
-        else {
-            right = mid - 1;  // Narrow the search to the left half
-        }
+    int low = 0, high = n-1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        // Check if x is present at mid
+        if (arr[mid] == key)
+            return mid;
+
+        // If x greater, ignore left half
+        if (arr[mid] < key)
+            low = mid + 1;
+
+        // If x is smaller, ignore right half
+        else
+            high = mid - 1;
     }
-    return -1;  // Return -1 if the key is not found
+
+    // If we reach here, then element was not present
+    return -1;
 }
 
 int main() {
